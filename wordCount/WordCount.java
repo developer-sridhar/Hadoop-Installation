44ef1c25d740b50e6e8add07a98ec37922f227ac
developer-sridhar <140785207+developer-sridhar@users.noreply.github.com>
2025-07-02 10:33:07 +0530

    Update WordCount.java

--- wordCount/WordCount.java
+++ wordCount/WordCount.java
@@ -1,21 +1,44 @@
 package com.example.wordcount;
 
-import java.io.IOException;
-
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
-public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-
-    private IntWritable result = new IntWritable();
-
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-        int sum = 0;
-        for (IntWritable val : values) {
-            sum += val.get();
+public class WordCount {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        // This line allows command line arguments to override configurations
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length < 2) {
+            System.err.println("Usage: wordcount <in> [<in>...] <out>");
+            System.exit(2);
         }
-        result.set(sum);
-        context.write(key, result);
+
+        Job job = Job.getInstance(conf, "word count"); // Use Job.getInstance(conf, name)
+
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(WordCountMapper.class);
+        // Optional: Use Combiner for local aggregation before shuffling
+        job.setCombinerClass(WordCountReducer.class); 
+        job.setReducerClass(WordCountReducer.class);
+
+        // Set output key and value types for the Reducer
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        // Add input paths
+        for (int i = 0; i < otherArgs.length - 1; ++i) {
+            FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
+        }
+        // Set output path (always the last argument)
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[otherArgs.length - 1]));
+
+        // Submit the job and wait for it to complete
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
